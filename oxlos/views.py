5df55b0f249854a9d45055f1cbcf98dfac5e7609
Patrick Altman <paltman@gmail.com>
2017-04-14 22:24:39 -0500

    Send is_member context var to templates

--- oxlos/views.py
+++ oxlos/views.py
@@ -19,22 +19,32 @@ def dashboard(request):
 @login_required
 def project(request, pk):
     project = get_object_or_404(Project, pk=pk)
-    return render(request, "project.html", {"project": project})
+    return render(request, "project.html", {
+        "project": project,
+        "is_member": project.team.is_member(request.user)
+    })
 
 
 @login_required
 def task(request, pk):
     task = get_object_or_404(Task, pk=pk)
-    return render(request, "task.html", {"task": task})
+    return render(request, "task.html", {
+        "task": task,
+        "is_member": task.project.team.is_member(request.user)
+    })
 
 
 @login_required
 def item(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    if request.method == "POST":
+    is_member = item.task.project.team.is_member(request.user)
+    if request.method == "POST" and is_member:
         item.add_answer(by=request.user, data=request.POST.getlist("answers"))
         return redirect("item", kwargs={"pk": task.item_set.order_by("?").first().pk})
-    return render(request, "item.html", {"item": item})
+    return render(request, "item.html", {
+        "item": item,
+        "is_member": is_member
+    })
 
 
 @login_required
