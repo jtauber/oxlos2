d2fabe86032504500cdc20066e6206f82de060f3
Patrick Altman <paltman@gmail.com>
2017-04-15 02:04:54 -0500

    Fix up logging

--- oxlos/settings.py
+++ oxlos/settings.py
@@ -164,23 +164,15 @@ PINAX_WEBANALYTICS_SETTINGS = {
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
-    "filters": {
-        "require_debug_false": {
-            "()": "django.utils.log.RequireDebugFalse"
-        }
-    },
     "handlers": {
-        "mail_admins": {
-            "level": "ERROR",
-            "filters": ["require_debug_false"],
-            "class": "django.utils.log.AdminEmailHandler"
+        "console": {
+            "class": "logging.StreamHandler",
         }
     },
     "loggers": {
-        "django.request": {
-            "handlers": ["mail_admins"],
-            "level": "ERROR",
-            "propagate": True,
+        "": {
+            "handlers": ["console"],
+            "level": "ERROR"
         },
     }
 }
