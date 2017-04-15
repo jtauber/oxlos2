bf05affcbb95d39d7aff5780adbf7e5b479520cd
James Tauber <jtauber@jtauber.com>
2017-04-14 23:43:33 -0400

    tweaked converted to be easier to use as library

--- data/convert_to_json.py
+++ data/convert_to_json.py
@@ -4,7 +4,9 @@ import json
 import os.path
 
 
-def convert(filename):
+def convert():
+    directory = os.path.dirname(os.path.abspath(__file__))
+    filename = os.path.join(directory, "mis-parse.txt")
 
     with open(filename) as f:
 
@@ -22,9 +24,5 @@ def convert(filename):
                 "choices": choices,
             }
             items.append(item)
-    return json.dumps(items)
 
-
-if __name__ == "__main__":
-    directory = os.path.dirname(os.path.abspath(__file__))
-    print(convert(os.path.join(directory, "mis-parse.txt")))
+    return items
