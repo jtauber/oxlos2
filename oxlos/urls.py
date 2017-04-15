a03b31cfec13782eca17fee901da8fbbd6a4d244
Patrick Altman <paltman@gmail.com>
2017-04-14 21:55:27 -0500

    View/template stubs

--- oxlos/urls.py
+++ oxlos/urls.py
@@ -1,16 +1,28 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 from django.contrib import admin
 
+from .views import (
+    home,
+    dashboard,
+    item,
+    item_random,
+    project,
+    task
+)
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r"^$", home, name="home"),
+    url(r"^dashboard/$", dashboard, name="dashboard"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
-    url(r"^teams/", include("pinax.teams.urls"))
+    url(r"^teams/", include("pinax.teams.urls")),
+    url(r"^projects/(?P<pk>\d+)/$", project, name="project"),
+    url(r"^tasks/(?P<pk>\d+)/$", task, name="task"),
+    url(r"^tasks/(?P<pk>\d+)/random-item/$", item_random, name="item_random"),
+    url(r"^items/(?P<pk>\d+)/$", item, name="item"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ oxlos/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import redirect, render, get_object_or_404
+
+from account.decorators import login_required
+
+from .models import Project, Task, Item
+
+
+def home(request):
+    if request.is_authenticated:
+        return redirect("dashboard")
+    return render("homepage.html")
+
+
+@login_required
+def dashboard(request):
+    return render(request, "dashboard.html", {"projects": Project.objects.all()})
+
+
+@login_required
+def project(request, pk):
+    project = get_object_or_404(Project, pk=pk)
+    return render(request, "project.html", {"project": project})
+
+
+@login_required
+def task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return render(request, "task.html", {"task": task})
+
+
+@login_required
+def item(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    if request.method == "POST":
+        pass  # @@@ handle creating answer and redirecting to next item
+    return render(request, "item.html", {"item": item})
+
+
+@login_required
+def item_random(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    return redirect("item", kwargs={"pk": task.item_set.order_by("?").first().pk})
