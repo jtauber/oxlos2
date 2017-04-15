fc2281249e27d339bd10937b7b1a24e9a1d02d98
James Tauber <jtauber@jtauber.com>
2017-04-14 22:43:15 -0400

    initial admin

---
+++ oxlos/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from .models import Project, Task, Item, ItemResponse
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "project"]
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["id", "task", "data"]
+
+
+@admin.register(ItemResponse)
+class ItemResponseAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "item", "created_at", "answer"]
+    list_filter = ["user"]
